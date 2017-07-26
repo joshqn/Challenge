6f7acc6c304d0b18e653c7b8c6304eafbbabe27b
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-26 15:09:31 -0500

    Created DetailVC and added TapGestureRecognizer

---
+++ Challenge/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Challenge
+//
+//  Created by JoshuaKuehn on 7/26/17.
+//
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      createAndAddGestureRecognizer()
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+  
+  func createAndAddGestureRecognizer() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(closeButtonTapped(_:)))
+    tapGesture.cancelsTouchesInView = false
+    tapGesture.delegate = self
+    view.addGestureRecognizer(tapGesture)
+  }
+  @IBAction func closeButtonTapped(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
+}
+
+// MARK: UIGestureRecognizerDelegate
+extension DetailViewController: UIGestureRecognizerDelegate {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+    return (touch.view === self.view)
+  }
+}
