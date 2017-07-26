1ee3d966c63e038bcd2b715b67a7dc9310a3dd57
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-26 16:28:29 -0500

    Hooked up the Search layer to the SearchVC

--- Challenge/SearchViewController.swift
+++ Challenge/SearchViewController.swift
@@ -16,6 +16,18 @@ class SearchViewController: UIViewController {
   var lineSpacing:CGFloat = 8
   var minInterSpacing:CGFloat = 8
   let detailSegueIdentifier = "ModalDetail"
+  var page = 0
+  
+  var startingNumberOfCells = 0
+  var endingNumberOfCells = 0
+  
+  var imageResults:[ImageResult] = [] {
+    didSet {
+      startingNumberOfCells = oldValue.count
+      endingNumberOfCells = imageResults.count
+    }
+  }
+  var isLoading = false
   
   override func viewDidLoad() {
       super.viewDidLoad()
@@ -28,11 +40,84 @@ class SearchViewController: UIViewController {
       super.didReceiveMemoryWarning()
   }
   
+  func searchForImageWith(name: String) {
+    Search.performSearchFor(imageName: name, page: page) { (isSuccessful, results) in
+      
+      // We're done loading the results of our search
+      self.isLoading = false
+      
+      // If our search is succesful execute this block
+      if isSuccessful {
+        
+        // Verify we got results
+        if let results = results {
+          // Append the new results to our imageResults array
+          self.imageResults += results
+          // Do a Batch update of inserts into the collectionView
+          self.collectionView.performBatchUpdates({
+            // Grab the indexPaths that are being inserted
+            let indexPaths = self.createIndexPathsToInsertIntoCollectionView()
+            self.collectionView.insertItems(at: indexPaths)
+          }, completion: nil)
+        } else {
+          // If results is nil then there are no more results so we display this alert
+          self.noMoreResultsAlert()
+        }
+      } else {
+        // Indicate there is an issue with the network
+        self.networkAlert()
+      }
+      
+    }
+  }
+  
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == detailSegueIdentifier {
       let detailVC = segue.destination as! DetailViewController
       let indexPath = sender as! IndexPath
     }
+  }
+  
+  // MARK: Helper Functions
+  func createIndexPathsToInsertIntoCollectionView() -> [IndexPath] {
+    var indexPaths: [IndexPath] = []
+    
+    for i in startingNumberOfCells..<endingNumberOfCells {
+      let indexPath = IndexPath(item: i, section: 0)
+      indexPaths.append(indexPath)
+    }
+    
+    return indexPaths
+  }
+  
+  func IndexPathsToBeDeleted() -> [IndexPath] {
+    var indexPaths:[IndexPath] = []
+    
+    for i in 0..<endingNumberOfCells {
+      let indexPath = IndexPath(item: i, section: 0)
+      indexPaths.append(indexPath)
+    }
+    return indexPaths
+  }
+  
+  // MARK: Alert Controllers
+  func networkAlert() {
+    let alert = UIAlertController(title: "Whooops...", message: "Looks like there's an error with the network. Try again later.", preferredStyle: .alert)
+    let okButton = UIAlertAction(title: "OK", style: .cancel)
+    
+    alert.addAction(okButton)
+    
+    present(alert, animated: true, completion: nil)
+    
+  }
+  
+  func noMoreResultsAlert() {
+    let alert = UIAlertController(title: "That's all of them", message: "Looks like theres no other pictures. Try searching for something else", preferredStyle: .alert)
+    let okButton = UIAlertAction(title: "OK", style: .cancel)
+    
+    alert.addAction(okButton)
+    
+    present(alert, animated: true, completion: nil)
   }
   
 }
@@ -55,7 +140,14 @@ extension SearchViewController: UICollectionViewDataSource {
   
   func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
     
+    // If the next row to be displayed is the last and nothing is loading then search for more images
+    if imageResults.count - indexPath.row < 2 && isLoading == false {
+      isLoading = true
+      searchForImageWith(name: searchBar.text!)
+    }
+    
   }
+  
 }
 
 //MARK: UICollectionViewDelegate
@@ -93,6 +185,17 @@ extension SearchViewController: UISearchBarDelegate {
   }
   
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    print("Clicked")
+    isLoading = true
+    
+    // Check if the imageResults array is currently holding anything
+    if imageResults.count > 0 {
+      imageResults.removeAll()
+      collectionView.reloadData()
+    }
+    
+    page = 1
+    searchForImageWith(name: searchBar.text!)
+    
+    searchBar.resignFirstResponder()
   }
 }
