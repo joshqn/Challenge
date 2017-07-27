ebbd63ad1a219c31b9ae1a2438e81d57dcfbeb8e
Josh Kuehn <joshmkuehn@gmail.com>
2017-07-26 19:52:35 -0500

    Fixed asynch network problems

--- Challenge/ImageResultCollectionViewCell.swift
+++ Challenge/ImageResultCollectionViewCell.swift
@@ -13,7 +13,7 @@ class ImageResultCollectionViewCell: UICollectionViewCell {
   
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var nameTextField: UITextField!
-  weak var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+  var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
   private var requestReceipt: RequestReceipt?
   
   func configure(for imageResult: ImageResult) {
@@ -24,23 +24,22 @@ class ImageResultCollectionViewCell: UICollectionViewCell {
   
   // Programtic activity indicator setup
   private func setupActivityIndicator() {
-    if let activityIndicator = activityIndicator {
-      activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-      self.addSubview(activityIndicator)
-      // Center the view and activate the constarints
-      activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-      activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-      // Start the animation
-      activityIndicator.startAnimating()
-      activityIndicator.hidesWhenStopped = true
-    }
+    
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(activityIndicator)
+    // Center the view and activate the constarints
+    activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    // Start the animation
+    activityIndicator.startAnimating()
+    activityIndicator.hidesWhenStopped = true
+    
   }
   
   // Setup and putting the UI in a state for the image to be loaded
   private func setupCellUI() {
     self.layer.cornerRadius = 5.0
     self.layer.borderWidth = 1.0
-    self.layer.borderColor = UIColor.black.cgColor
     self.nameTextField.layer.isHidden = true
   }
   
@@ -48,11 +47,15 @@ class ImageResultCollectionViewCell: UICollectionViewCell {
   private func searchForImageWith(imageResult: ImageResult) {
     // Grab the requestReceipt so the cell can cancel the request is it's reused before finishing
     requestReceipt = Search.downloadImageWith(urlRequest: imageResult.urlRequest, withiD: imageResult.id, completion: { [weak self] isSuccessful in
-      self?.requestReceipt = nil
-      self?.activityIndicator?.stopAnimating()
-      self?.nameTextField.layer.isHidden = false
-      self?.imageView.image = Search.downloader.imageCache?.image(for: imageResult.urlRequest, withIdentifier: imageResult.id)
-      self?.nameTextField.text = imageResult.name
+      if isSuccessful {
+        self?.activityIndicator.stopAnimating()
+        self?.nameTextField.layer.isHidden = false
+        self?.imageView.image = Search.downloader.imageCache?.image(for: imageResult.urlRequest, withIdentifier: imageResult.id)
+        self?.nameTextField.text = imageResult.name
+        self?.layer.borderColor = UIColor.black.cgColor
+        self?.requestReceipt = nil
+      }
+      
     })
   }
   
